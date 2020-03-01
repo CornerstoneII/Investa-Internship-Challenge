6104b4d815e01f33a39179fc791f0b636e0f9000
Iyadi Ol'seun <42003510+CornerstoneII@users.noreply.github.com>
2020-03-01 15:30:22 -0800

    Added the files

---
+++ cc3.js
@@ -0,0 +1,33 @@
+
+// let todoItems = [];
+
+// function addTodo(text, title) {
+//   const todo = {
+//   	title,
+//     text,
+//     checked: false,
+//     id: Date.now(),
+//   };
+
+//   todoItems.push(todo);
+//   console.log(todoItems);
+// }
+
+// const form = document.querySelector('.js-form');
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const input = document.querySelector('.js-todo-input');
+
+//   const text = input.value.trim();
+//   const title = input.value.trim();
+//   if (text !== '') {
+//     addTodo(text);
+//     input.value = '';
+//     input.focus();
+//     if (title !== '') {
+//         addTodo(title);
+//         input.value = '';
+//         input.focus();
+//       }
+//   }
+// });
